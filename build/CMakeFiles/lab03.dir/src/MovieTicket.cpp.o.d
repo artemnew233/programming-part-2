812946b4e35bfb15ee733a1e3878cea817e92e8e
ArtemS <asmeliantsev.pp@gmail.com>
2026-04-12 17:16:10 +0200

    now ACTUALLY done with lab03

--- build/CMakeFiles/lab03.dir/src/MovieTicket.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/lab03.dir/src/MovieTicket.cpp.o: \
- /home/asmel/developer/programming-part-2/programming-part-2/src/MovieTicket.cpp \
- /usr/include/stdc-predef.h \
- /home/asmel/developer/programming-part-2/programming-part-2/include/MovieTicket.hpp
